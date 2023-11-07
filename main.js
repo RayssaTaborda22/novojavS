c3e42c6aeb301ddb41b4903b7b8fb0a7cf1e7cf3
RayssaTaborda22 <117189985+RayssaTaborda22@users.noreply.github.com>
2023-11-07 09:12:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,18 @@ while () {
 }
 let contador = 0;
 while(contador <8) {}
-while(contador <9) {
+while(contador <9) 
     listaDeTeclas[0].onclick = tocaSomPom;
     contador = 1;
+    console.log(contador);listaDeTeclas[contador].onclick = tocaSomPom;
+    function tocaSomPom(){
+        document.querySelector(‘#som_tecla_pom’).play();
+        while(contador <9)
+        while(contador < listaDeTeclas.length)
+        listaDeTeclas[contador].onclick = tocaSomPom;
+contador = contador + 1;
+
+
+
+
 }
